40a64a9a8db01eadd48ea833d579323d619e710e
dheeraj <dheerajpatidar20575@gmail.com>
2025-05-06 11:59:46 +0530

    updated settings

--- backend/kudos/kudos/settings.py
+++ backend/kudos/kudos/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = 'RENDER' not in os.environ
 
 # Allow your Render URL through
-ALLOWED_HOSTS = ["https://kudos-app-xubp.onrender.com"]  
+ALLOWED_HOSTS = ['kudos.onrender.com', 'localhost', '127.0.0.1']
+
 # Optional: Allow localhost during development
 if DEBUG:
     ALLOWED_HOSTS += ['localhost', '127.0.0.1']
