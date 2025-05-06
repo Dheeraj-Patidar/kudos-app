4aceb78ea6e1925e6c76e907d8a85bc7ac5db1b5
dheeraj <dheerajpatidar20575@gmail.com>
2025-05-06 16:00:55 +0530

    updated api url

--- frontend/kudos/src/Api.jsx
+++ frontend/kudos/src/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "http://localhost:8000/api";
 
-const BASE_URL = "api/"+import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 // Helper function to get access token
