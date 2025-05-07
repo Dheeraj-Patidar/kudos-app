94c9c44b8ae896ff9ec6424f31ea331be6ae8227
dheeraj <dheerajpatidar20575@gmail.com>
2025-05-07 14:12:15 +0530

    added postgres database

--- backend/kudos/kudos/settings.py
+++ backend/kudos/kudos/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 
 from celery.schedules import crontab
 from dotenv import load_dotenv
+import dj_database_url
+
+
 
 load_dotenv()  # Load environment variables from .env
 
@@ -105,10 +108,20 @@ WSGI_APPLICATION = "kudos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
-}
-
+# DATABASES = {
+#     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+# }
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
