9b8b10f3ffb66e70f528f6d9c3bc94cc0d50df47
dheeraj <dheerajpatidar20575@gmail.com>
2025-05-08 18:06:26 +0530

    make navbar responsive for mobile screen

--- frontend/kudos/src/components/Navbar.jsx
+++ frontend/kudos/src/components/Navbar.jsx
@@ -36,59 +36,69 @@ const Navbar = ({ kudosCount }) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        {(isAuthenticated ? (
-        <h5>Kudos Balance ({kudosCount})</h5>
-        ):("")
-      )}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  <div className="container">
+    <Link className="navbar-brand" to="/">Kudos App</Link>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+
+        {isAuthenticated && (
+          <>
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/kudos">Your Kudos</Link>
             </li>
-
-
             <li className="nav-item">
+              <Link className="nav-link" to="/givekudos">Give Kudos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/resetpassword">Reset Password</Link>
+            </li>
+          </>
+        )}
+      </ul>
 
-              {isAuthenticated ? (
-                <div className="d-flex">
-                <Link className="nav-link" to="/kudos">Your Kudos</Link>
-
-                <Link className="nav-link" to="/givekudos">Give-Kudos</Link>
-
-                <Link className="nav-link" to="/resetpassword">Reset-Password</Link>
-
-                <button
-                className="btn btn-danger"
-                onClick={handleLogout}
-              >
+      <ul className="navbar-nav ms-auto">
+        {isAuthenticated ? (
+          <>
+            <li className="nav-item d-flex align-items-center me-2">
+              <span className="text-white">Kudos Balance ({kudosCount})</span>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-danger" onClick={handleLogout}>
                 Logout
               </button>
-              </div>
-
-              ) : (
-                <div className="d-flex">
-                    <Link className="nav-link" to="/login">Login</Link>
-
-                    <Link className="nav-link" to="/signup">Sign-up</Link>
-
-                </div>
-              )}
             </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">Sign Up</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  </div>
+</nav>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
   );
 };
 
--- frontend/kudos/src/main.jsx
+++ frontend/kudos/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css"; // Import Toastify styles
 import { ToastContainer } from "react-toastify";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 createRoot(document.getElementById('root')).render(
